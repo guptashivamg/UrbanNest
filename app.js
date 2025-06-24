9eef2504688286247cffca7f38206efba50deaec
Shivam Gupta <guptajishiv12@gmail.com>
2025-06-24 14:13:35 +0530

    feat: configure MongoDB connection using environment variable for database URL

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ const User = require("./models/user.js");
 const userRouter = require("./routes/user.js");
 
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-// const dbUrl = process.env.ATLASDB_URL;
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -38,7 +38,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
